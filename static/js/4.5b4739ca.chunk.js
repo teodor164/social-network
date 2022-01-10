17dc7f3e5296ee9b581dd21351476ca4f90b6148
Maxim <maxim.rogozneak@gmail.com>
2022-01-10 15:26:26 +0200

    Updates

--- static/js/4.5b4739ca.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[4],{478:function(t,e,n){"use strict";n.r(e);n(0);var p=n(122),s=n(1),c=Object(p.a)((function(){return Object(s.jsx)("div",{children:"Settings"})}));e.default=c}}]);
-//# sourceMappingURL=4.5b4739ca.chunk.js.map
---
+++ static/js/4.b1ac462a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[4],{479:function(t,e,n){"use strict";n.r(e);n(0);var p=n(121),s=n(1),c=Object(p.a)((function(){return Object(s.jsx)("div",{children:"Settings"})}));e.default=c}}]);
+//# sourceMappingURL=4.b1ac462a.chunk.js.map
