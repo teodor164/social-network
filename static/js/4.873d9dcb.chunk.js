37fecbfb92d9d8fd807b48b7254e24f020751def
Maxim <maxim.rogozneak@gmail.com>
2022-01-12 10:52:17 +0200

    Updates

---
+++ static/js/4.873d9dcb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[4],{480:function(t,e,n){"use strict";n.r(e);n(0);var p=n(83),s=n(1),c=Object(p.a)((function(){return Object(s.jsx)("div",{children:"Settings"})}));e.default=c}}]);
+//# sourceMappingURL=4.873d9dcb.chunk.js.map
